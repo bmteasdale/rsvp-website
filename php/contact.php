f4886cad47ca004edd8a1aadda10d67fb8f2f35f
bmteasdale <bmteasdale3@gmail.com>
2020-01-25 10:53:54 -0400

    added contact php and PHPMailer

---
+++ php/contact.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('../libs/PHPMailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer();
+$mail -> isSMPT();
+$mail -> SMPTAuth = true;
+$mail -> SMTPSecure = 'tls';
+$mail -> Host = 'smpt.gmail.com';
+$mail -> Port = '587';
+$mail -> isHTML();
+$mail -> Username = 'bmteasdale.dev@gmail.com';
+$mail -> Password = 'ickeTche';
+$mail -> SetFrom('no-reply@rsvpweb.com');
+$mail -> Subject = 'Test';
+$mail -> Body = 'A test E-Mail';
+$mail -> AddAddress('bmteasdale3@gmail.com');
+
+$mail -> Send();
+?>
