ab99c928a13dbc4d00aa3dfee53228795f1baeec
bmteasdale <bmteasdale3@gmail.com>
2020-01-26 16:41:35 -0400

    removed PHPMailer

--- php/contact.php
+++ php/contact.php
@@ -1,20 +1,10 @@
 <?php
 
-require_once('../libs/PHPMailer/PHPMailerAutoload.php');
+// PHP Driver
+$mongo = new MongoDB\Client(
+    'mongodb+srv://RSVPAdmin:admin@rsvp-form-data-e9b0b.mongodb.net/test?retryWrites=true&w=majority');
 
-$mail = new PHPMailer();
-$mail -> isSMPT();
-$mail -> SMPTAuth = true;
-$mail -> SMTPSecure = 'ssl';
-$mail -> Host = 'smpt.gmail.com';
-$mail -> Port = '465';
-$mail -> isHTML();
-$mail -> Username = 'bmteasdale.dev@gmail.com';
-$mail -> Password = 'ickeTche';
-$mail -> SetFrom('no-reply@rsvpweb.com');
-$mail -> Subject = 'Test';
-$mail -> Body = 'A test E-Mail';
-$mail -> AddAddress('bmteasdale3@gmail.com');
 
-$mail -> Send();
+    
+$db = $mongo->test;
 ?>
