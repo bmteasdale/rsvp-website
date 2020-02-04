ecd276eeee2ac3ce6f392e30988b1207a65d2939
bmteasdale <bmteasdale3@gmail.com>
2020-02-03 20:57:20 -0400

    added Mongo Connection and serving more paths

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+const chalk = require('chalk');
+const rsvpRouter = require('./server/routes/rsvpRoute');
+const messageRouter = require('./server/routes/messageRoute')
+const dbName = "Completed-RSVP's"
 
 const app = express();
-app.use('/static', express.static('public'));
+
+// Use Static files
+app.use('/src', express.static(__dirname + '/src'));
+
+// MongoDB Atlas connection URI
+mongoose.connect('mongodb+srv://RSVPAdmin:admin@rsvp-form-data-e9b0b.mongodb.net/'+ dbName +'?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 // render /
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
-console.log(__dirname);
-
+    res.sendFile(__dirname + '/public/index.html');
+});
 // render /index.html
 app.get('/index.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
-})
-
+    res.sendFile(__dirname + '/public/index.html');
+});
 // render /contact.html
 app.get('/contact.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/contact.html'));
-})
+    res.sendFile(__dirname + '/public/contact.html');
+});
 
-app.listen(8000);
+//render /submission.html on RSVP form submission
+app.use(rsvpRouter);
+app.get('/rsvp-submit', (req, res) => {
+    res.sendFile(__dirname + '/public/submission.html');
+});
+
+//render /message.html on contact form submission
+app.use(messageRouter);
+app.get('/message-submit', (req,res) => {
+    res.sendFile(__dirname + '/public/message.html');
+});
+
+// localhost:8000
+app.listen(8000, () => { console.log(chalk.greenBright.inverse.bold('Server is running...')) });
