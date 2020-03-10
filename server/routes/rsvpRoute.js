e60090a6223505ae0a4fafff20f5b092e91457b6
bmteasdale <bmteasdale3@gmail.com>
2020-03-10 08:49:59 -0300

    updated for new meal selections

--- server/routes/rsvpRoute.js
+++ server/routes/rsvpRoute.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongo = require('mongodb').MongoClient;
 const RSVPModel = require('../models/rsvpSchema');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.post('/rsvp-submit', async (req, res) => {
-    var date_ob = new Date();
-    var Day = date_ob.getDate();
-    var Month = date_ob.getMonth() + 1;
-    var Year = date_ob.getFullYear();
 
-    var CurrentDate = Year + '/' + Month + '/' + Day;
-
-    var RSVP = new RSVPModel({
+    var rsvp = new RSVPModel({
         full_name: req.body.fullName,
         attendingStatus: req.body.attendance,
-        meal_selection: req.body.mealChoice,
-        additional_guests: req.body.additionalGuests,
+        roast_beef_qty: req.body.roastBeefQty,
+        salmon_qty: req.body.salmonQty,
+        chicken_qty: req.body.chickenQty,
+        lasagna_qty: req.body.lasagnaQty,
+        guest_names: req.body.additionalGuests,
         song_requests: req.body.songRequests,
-        date_completed: CurrentDate
+        message: req.body.rsvpMessage,
+        date_completed: new Date()
     });
 
     try {
-        await RSVP.save();
+        await rsvp.save();
         res.redirect('/rsvp-submit');
     } catch (err) {
         res.status(500).send(err);
