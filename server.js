aa29a623a2d7d538efa905ba9c027cf3924f12c3
bmteasdale <bmteasdale3@gmail.com>
2020-10-07 13:42:17 -0300

    add startup debug message

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+console.log("Server.js started.")
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
+console.log("Dependencies Enviroment Loaded")
 
 const app = express();
 
